f2bcb768175e2eb148f8831cd246dceab449d727
retrouvailles0 <retrouvailles0@hotmail.com>
2023-11-28 13:39:37 -0800

    map view & controller & navigation added

---
+++ FitTogether/Map Screen/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  FitTogether
+//
+//  Created by retrouvailles on 11/28/23.
+//
+
+import UIKit
+
+class MapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
