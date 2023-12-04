c3b1c38da915fccfc50e300607c4898bed1e6bf2
retrouvailles0 <retrouvailles0@hotmail.com>
2023-12-04 12:17:33 -0800

    add share friends button & view & controller

---
+++ FitTogether/Friends Chat Screen/FriendsChatView.swift
@@ -0,0 +1,20 @@
+//
+//  FriendsChatView.swift
+//  FitTogether
+//
+//  Created by retrouvailles on 12/3/23.
+//
+
+import UIKit
+
+class FriendsChatView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
